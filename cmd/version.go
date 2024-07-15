cf40bd73df13c84c86a3c8614b0f9f45e3483e48
Alexander Hunter <icehuntmen@gmail.com>
2024-07-15 12:36:54 +0300

    Update script

--- cmd/version.go
+++ cmd/version.go
@@ -69,6 +69,8 @@ var checkCmd = &cobra.Command{
 	},
 }
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func main() {
 	var rootCmd = &cobra.Command{Use: "app"}
 	rootCmd.AddCommand(VersionCmd)
