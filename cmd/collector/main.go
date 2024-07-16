3d74da721a086b30c0ee7211867505fb9ffc0aa8
Cold927 <colddevelop@gmail.com>
2024-07-16 10:50:34 +0300

    Login http request

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"crm-glonass/config"
-	"crm-glonass/pkg/logging"
+	"bytes"
+	"fmt"
+	"net/http"
 )
 
 func main() {
+	// URL, куда будем отправлять POST-запрос
+	url := "https://hosting.glonasssoft.ru/api/v3/auth/login"
 
-	// Start server
-	logger := logging.NewLogger(config.GetConfig())
-	logger.Info(logging.General, logging.StartUp, "Started server...", map[logging.ExtraKey]interface{}{"Version": config.GetConfig().Version})
+	// Тело POST-запроса в формате JSON
+	jsonBody := []byte(`{
+  		"login": "demo",
+  		"password": "%%demo%%"
+	}`)
 
+	// Создаем новый запрос типа POST
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Устанавливаем заголовок Content-Type для указания типа содержимого
+	req.Header.Set("Content-Type", "application/json")
+
+	// Создаем HTTP-клиент
+	client := &http.Client{}
+
+	// Отправляем запрос и получаем ответ
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Выводим статус код ответа
+	fmt.Println("Response Status:", resp.Status)
 }
