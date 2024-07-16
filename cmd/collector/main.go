5a83ab90cdb13a9dd36306fe2a83ede22884705d
Cold927 <colddevelop@gmail.com>
2024-07-16 13:20:57 +0300

    Login data response, using headers

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -2,41 +2,62 @@ package main
 
 import (
 	"bytes"
+	"crm-glonass/config"
+	"crm-glonass/pkg/logging"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func main() {
-	// URL, куда будем отправлять POST-запрос
-	url := "https://hosting.glonasssoft.ru/api/v3/auth/login"
+type Post struct {
+	AuthId        string `json:"AuthId"`
+	UserId        string `json:"UserId"`
+	User          string `json:"User"`
+	Notifications bool   `json:"Notifications"`
+}
 
-	// Тело POST-запроса в формате JSON
-	jsonBody := []byte(`{
-  		"login": "demo",
+var logger = logging.NewLogger(config.GetConfig())
+
+func main() {
+	posturl := "https://hosting.glonasssoft.ru/api/v3/auth/login"
+
+	body := []byte(`{
+		"login": "demo",
   		"password": "%%demo%%"
 	}`)
 
-	// Создаем новый запрос типа POST
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+	r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(body))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
+		panic(err)
 	}
 
-	// Устанавливаем заголовок Content-Type для указания типа содержимого
-	req.Header.Set("Content-Type", "application/json")
+	r.Header.Add("Content-Type", "application/json")
+	r.Header.Add("User-Agent", "")
 
-	// Создаем HTTP-клиент
 	client := &http.Client{}
-
-	// Отправляем запрос и получаем ответ
-	resp, err := client.Do(req)
+	res, err := client.Do(r)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
+		panic(err)
 	}
-	defer resp.Body.Close()
+	defer res.Body.Close()
 
-	// Выводим статус код ответа
-	fmt.Println("Response Status:", resp.Status)
+	if res.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("unexpected status code: %d", res.StatusCode))
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response Body:", string(bodyBytes))
+
+	post := &Post{}
+	err = json.Unmarshal(bodyBytes, post)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Debugf("Auth: %s, UserId: %s, Username: %s", post.AuthId, post.UserId, post.User)
 }
